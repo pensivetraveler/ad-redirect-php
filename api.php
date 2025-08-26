bef1744e501b61d567fcf5f4449b284cbffd87d6
pensivetraveler <pensivetraveler@gmail.com>
2025-08-26 17:13:23 +0900

    feat: APP_KEY 값으로 제한 걸어둠

--- api.php
+++ api.php
@@ -1,30 +1,9 @@
 <?php
-require "common.php";
-header('Content-Type: application/json');
+if(!isset($_ENV['APP_KEY'])) require 'common.php';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);
     echo json_encode(['status' => 'fail', 'reason' => 'method_not_allowed']);
-    exit;
-}
-
-$input = json_decode(file_get_contents('php://input'), true);
-$isIOS = !empty($input['isIOS']) && $input['isIOS'] === true;
-
-if ($isIOS) {
-    // iOS iframe 전용 처리 (세션 없이)
-    if (!isset($input['token'])) {
-        $newToken = bin2hex(random_bytes(16));
-        echo json_encode(['status'=>'retry','token'=>$newToken]);
-        exit;
-    }
-
-    if (isBot() || isLikelyBot(getClientIp())) {
-        echo json_encode(['status'=>'fail','reason'=>'bot_detected']);
-        exit;
-    }
-
-    echo json_encode(['status'=>'ok']);
     exit;
 }
 
